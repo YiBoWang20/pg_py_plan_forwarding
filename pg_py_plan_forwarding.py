1ad15009615c23e0f41097b0c2b721c375a2bd99
Eduards Gruberts <eduards.gruberts@gogetssl.com>
2022-03-31 12:56:56 +0300

    update pg_py_plan_forwarding.py

--- pg_py_plan_forwarding.py
+++ pg_py_plan_forwarding.py
@@ -25,4 +25,30 @@ def init():
 	syslog.syslog("init")
 
 
+def executor_start(query_desc):
+	syslog.syslog("executor_start")
+
+	file_object = open('/home/vagrant/pg_py_plan_forwarding/planner_output.txt', 'a')
+
+	file_object.write('parse')
+	file_object.write('\n')
+
+	file_object.write(query_desc)
+	file_object.write('\n')
+
+	file_object.close()
+
+def executor_end(query_desc):
+	syslog.syslog("executor_end")
+
+	file_object = open('/home/vagrant/pg_py_plan_forwarding/planner_output.txt', 'a')
+
+	file_object.write('parse')
+	file_object.write('\n')
+
+	file_object.write(query_desc)
+	file_object.write('\n')
+
+	file_object.close()
+
 syslog.syslog("main")
