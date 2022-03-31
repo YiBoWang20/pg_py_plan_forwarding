2803a943ab0dc33b4e0be8fe562f92469f18f375
sk <eduards.gruberts@gmial.com>
2022-03-31 11:22:03 +0300

    modyfy pg_py_plan_forwarding.py

--- pg_py_plan_forwarding.py
+++ pg_py_plan_forwarding.py
@@ -1,8 +1,24 @@
 import syslog
 
 def planner(parse, planned_stmt):
-	syslog.syslog(parse)
-	syslog.syslog(planned_stmt)
+	syslog.syslog('planner')
+
+	file_object = open('/home/vagrant/pg_py_plan_forwarding/planner_output.txt', 'a')
+
+	file_object.write('parse')
+	file_object.write('\n')
+	
+	file_object.write(parse)
+	file_object.write('\n')
+
+	file_object.write('planned_stmt')
+	file_object.write('\n')
+
+	file_object.write(planned_stmt)
+	file_object.write('\n')
+
+	file_object.close()
+
 	return planned_stmt
 
 def init():
